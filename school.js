792193dded6004f1f27a791d69166e749cf2dda7
Pedro Alves <3pedru@gmail.com>
2016-01-06 10:51:22 -0200

    preciso de ajuda

--- school.js
+++ school.js
@@ -1,145 +1,139 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf340
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 \{\
-  "_id": 1,\
-  "zipcode": "63109",\
-  "students": [\
-    \{\
-      "name": "john",\
-      "school": 102,\
-      "age": 10,\
-      "class": \{\
-        "teacher": "lennon",\
-        "book": [\
-          \{\
-            "pages": 32,\
-            "title": "a"\
-          \},\
-          \{\
-            "pages": 45,\
-            "title": "b"\
-          \},\
-          \{\
-            "pages": 32,\
-            "title": "c"\
-            \
-          \}\
-        ],\
-        "paper": [\
-          \{\
-            "color": "white"\
-          \},\
-          \{\
-            "color": "blue"\
-          \},\
-          \{\
-            "color": "red"\
-          \}\
-        ],\
-        "things": [\
-          \{\
-            "pencil": "red"\
-          \},\
-          \{\
-            "pen": "blue"\
-          \},\
-          \{\
-            "eraser": "white"\
-          \}\
-        ]\
-      \}\
-    \},\
-    \{\
-      "name": "jorge",\
-      "school": 102,\
-      "age": 10,\
-      "class": \{\
-        "teacher": "harrison",\
-        "book": [\
-          \{\
-            "pages": 32,\
-            "title": "d"\
-          \},\
-          \{\
-            "pages": 20,\
-            "title": "e"\
-          \},\
-          \{\
-            "pages": 188,\
-            "title": "f"\
-          \}\
-        ],\
-        "paper": [\
-          \{\
-            "color": "white"\
-          \},\
-          \{\
-            "color": "blue"\
-          \},\
-          \{\
-            "color": "red"\
-          \}\
-        ],\
-        "things": [\
-          \{\
-            "pencil": "red"\
-          \},\
-          \{\
-            "pen": "blue"\
-          \},\
-          \{\
-            "eraser": "white"\
-          \}\
-        ]\
-      \}\
-    \},\
-    \{\
-      "name": "ringo",\
-      "school": 102,\
-      "age": 10,\
-      "class": \{\
-        "teacher": "star",\
-        "book": [\
-          \{\
-            "pages": 55,\
-            "title": "g"\
-          \},\
-          \{\
-            "pages": 66,\
-            "title": "h"\
-          \},\
-          \{\
-            "pages": 321,\
-            "title": "i"\
-          \}\
-        ],\
-        "paper": [\
-          \{\
-            "color": "white"\
-          \},\
-          \{\
-            "color": "blue"\
-          \},\
-          \{\
-            "color": "red"\
-          \}\
-        ],\
-        "things": [\
-          \{\
-            "pencil": "red"\
-          \},\
-          \{\
-            "pen": "blue"\
-          \},\
-          \{\
-            "eraser": "white"\
-          \}\
-        ]\
-      \}\
-    \}\
-  ]\
-\}}
+{
+  "_id": 1,
+  "zipcode": "63109",
+  "students": [
+    {
+      "name": "john",
+      "school": 102,
+      "age": 10,
+      "class": {
+        "teacher": "lennon",
+        "book": [
+          {
+            "pages": 32,
+            "title": "a"
+          },
+          {
+            "pages": 45,
+            "title": "b"
+          },
+          {
+            "pages": 32,
+            "title": "c"
+            
+          }
+        ],
+        "paper": [
+          {
+            "color": "white"
+          },
+          {
+            "color": "blue"
+          },
+          {
+            "color": "red"
+          }
+        ],
+        "things": [
+          {
+            "pencil": "red"
+          },
+          {
+            "pen": "blue"
+          },
+          {
+            "eraser": "white"
+          }
+        ]
+      }
+    },
+    {
+      "name": "jorge",
+      "school": 102,
+      "age": 10,
+      "class": {
+        "teacher": "harrison",
+        "book": [
+          {
+            "pages": 32,
+            "title": "d"
+          },
+          {
+            "pages": 20,
+            "title": "e"
+          },
+          {
+            "pages": 188,
+            "title": "f"
+          }
+        ],
+        "paper": [
+          {
+            "color": "white"
+          },
+          {
+            "color": "blue"
+          },
+          {
+            "color": "red"
+          }
+        ],
+        "things": [
+          {
+            "pencil": "red"
+          },
+          {
+            "pen": "blue"
+          },
+          {
+            "eraser": "white"
+          }
+        ]
+      }
+    },
+    {
+      "name": "ringo",
+      "school": 102,
+      "age": 10,
+      "class": {
+        "teacher": "star",
+        "book": [
+          {
+            "pages": 55,
+            "title": "g"
+          },
+          {
+            "pages": 66,
+            "title": "h"
+          },
+          {
+            "pages": 321,
+            "title": "i"
+          }
+        ],
+        "paper": [
+          {
+            "color": "white"
+          },
+          {
+            "color": "blue"
+          },
+          {
+            "color": "red"
+          }
+        ],
+        "things": [
+          {
+            "pencil": "red"
+          },
+          {
+            "pen": "blue"
+          },
+          {
+            "eraser": "white"
+          }
+        ]
+      }
+    }
+  ]
+}
